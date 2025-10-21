6aafff4d62730db2c847657f30326a08433f9011
Artyom Platto <platar.ant@yandex.ru>
2025-10-21 03:54:26 +0300

    seter

--- internal/entity/payments.go
+++ internal/entity/payments.go
@@ -1,14 +1,18 @@
 package entity
 
-type Payments struct {
-	ID         uint
-	CheckID    uint
-	FromPerson Persons
-	ToPerson   Persons
-	Amount     float64
-	Status     bool
+type payments struct {
+	id         uint
+	checkID    uint
+	fromPerson *Persons
+	toPerson   *Persons
+	amount     float64
+	status     bool
 }
 
 //type PersonInput struct {
 //
 //}
+
+func New(ID uint, CheckID uint, FromPerson *Persons, ToPerson *Persons, Amount float64, Status bool) *payments {
+	return &payments{id: ID, checkID: CheckID, fromPerson: FromPerson, toPerson: ToPerson, amount: Amount, status: Status}
+}

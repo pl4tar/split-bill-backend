afeb7bc991c8cac7802aca208a738d373190f696
Platto Artyom <platar.ant@yandex.ru>
2025-11-11 02:26:50 +0300

    feat: add cors policy

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -2,6 +2,7 @@ package handler
 
 import (
 	"context"
+	"log/slog"
 	"net/http"
 	"split-bill-backend/config"
 	"split-bill-backend/internal/handler/controllers"
@@ -11,28 +12,72 @@ func Setup(cfg *config.Config, ctx context.Context) http.Handler {
 	mux := http.NewServeMux()
 	db := cfg.Client
 	//Users
-	mux.HandleFunc(GetUserByEmail, controllers.GetUserByEmail(ctx, db))
-	mux.HandleFunc(PostNewUser, controllers.AddNewUser(ctx, cfg))
-	mux.HandleFunc(DeleteUserByEmail, controllers.DeleteUserEmail(ctx, db))
+	mux.HandleFunc(GetUserByEmail, loggingMiddleware(corsMiddleware(controllers.GetUserByEmail(ctx, db))))
+	mux.HandleFunc(PostNewUser, loggingMiddleware(corsMiddleware(controllers.AddNewUser(ctx, cfg))))
+	mux.HandleFunc(DeleteUserByEmail, loggingMiddleware(corsMiddleware(controllers.DeleteUserEmail(ctx, db))))
 
 	//Bills
-	mux.HandleFunc(GetAllBill, controllers.GetAllBillsHandler(ctx, db))
-	mux.HandleFunc(PostNewBill, controllers.AddNewBill(ctx, cfg))
-	mux.HandleFunc(DeleteBillByID, controllers.DeleteBillByID(ctx, db))
-	mux.HandleFunc(UpdateBillTitle, controllers.EditBill(ctx, db))
+	mux.HandleFunc(GetAllBill, loggingMiddleware(corsMiddleware(controllers.GetAllBillsHandler(ctx, db))))
+	mux.HandleFunc(PostNewBill, loggingMiddleware(corsMiddleware(controllers.AddNewBill(ctx, cfg))))
+	mux.HandleFunc(DeleteBillByID, loggingMiddleware(corsMiddleware(controllers.DeleteBillByID(ctx, db))))
+	mux.HandleFunc(UpdateBillTitle, loggingMiddleware(corsMiddleware(controllers.EditBill(ctx, db))))
 
 	//Persons
-	mux.HandleFunc(GetAllPersons, controllers.GetAllPersonsHandler(ctx, db))
-	mux.HandleFunc(PostNewPerson, controllers.AddNewPersonHandler(ctx, cfg))
-	mux.HandleFunc(DeletePerson, controllers.DeletePersonByID(ctx, db))
-	mux.HandleFunc(UpdatePersonName, controllers.EditPersonHandler(ctx, db))
+	mux.HandleFunc(GetAllPersons, loggingMiddleware(corsMiddleware(controllers.GetAllPersonsHandler(ctx, db))))
+	mux.HandleFunc(PostNewPerson, loggingMiddleware(corsMiddleware(controllers.AddNewPersonHandler(ctx, cfg))))
+	mux.HandleFunc(DeletePerson, loggingMiddleware(corsMiddleware(controllers.DeletePersonByID(ctx, db))))
+	mux.HandleFunc(UpdatePersonName, loggingMiddleware(corsMiddleware(controllers.EditPersonHandler(ctx, db))))
 
 	// Products
-	mux.HandleFunc(GetAllProducts, controllers.GetAllProductsHandler(ctx, db))
-	mux.HandleFunc(PostNewProduct, controllers.AddNewProductHandler(ctx, db))
-	mux.HandleFunc(DeleteProduct, controllers.DeleteProductByID(ctx, db))
+	mux.HandleFunc(GetAllProducts, loggingMiddleware(corsMiddleware(controllers.GetAllProductsHandler(ctx, db))))
+	mux.HandleFunc(PostNewProduct, loggingMiddleware(corsMiddleware(controllers.AddNewProductHandler(ctx, db))))
+	mux.HandleFunc(DeleteProduct, loggingMiddleware(corsMiddleware(controllers.DeleteProductByID(ctx, db))))
 
 	// Calculation
-	mux.HandleFunc(CalculateDebts, controllers.CalculateDebtsHandler(ctx, cfg.Client))
+	mux.HandleFunc(CalculateDebts, loggingMiddleware(corsMiddleware(controllers.CalculateDebtsHandler(ctx, cfg.Client))))
 	return mux
+}
+
+func loggingMiddleware(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		// Logging every request
+
+		// не логируем метод OPTIONS
+		if r.Method == http.MethodOptions {
+			next(w, r)
+			return
+		}
+
+		ip := r.Header.Get("X-Forwarded-For")
+
+		userAgent := r.Header.Get("User-Agent")
+		slog.Info("IP: %s, Method: %s, Route: %s, Query: %s, UserAgent: %s, AuthHeader: %s",
+			ip, r.Method, r.URL.Path, r.URL.Query(), userAgent, r.Header.Get("Authorization"))
+
+		next(w, r)
+	}
+}
+
+func corsMiddleware(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		allowedOrigins := map[string]bool{
+			"http://localhost:5173": true,
+		}
+		origin := r.Header.Get("Origin")
+		if allowedOrigins[origin] {
+			//w.Header().Set("Access-Control-Allow-Origin", origin)
+			w.Header().Set("Access-Control-Allow-Credentials", "true")
+		}
+
+		w.Header().Set("Access-Control-Allow-Origin", origin)
+		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, PATCH")
+		w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, X-Requested-With")
+
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	}
 }
